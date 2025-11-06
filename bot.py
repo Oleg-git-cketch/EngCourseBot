48cbca1302feefe39c2e3fea93572d12412bdb29
Oleg-git-cketch <aliol439003@gmail.com>
2025-11-06 10:59:44 +0500

    UpdateLinks

--- bot.py
+++ bot.py
@@ -30,23 +30,23 @@ def callback(call):
 
     if call.data == 'pre_intermediate':
 
-        bot.send_message(call.message.chat.id, 'Pre_intermediate audio', reply_markup=one_button('https://t.me/+i39y2WNngXFmM2Yy'))
-        bot.send_message(call.message.chat.id, 'Pre_intermediate video', reply_markup=one_button('https://t.me/+LxKixadK_mYxMDgy'))
+        bot.send_message(call.message.chat.id, 'Pre_intermediate audio', reply_markup=one_button('https://t.me/+EQAHWsKgkMRiNTIy'))
+        bot.send_message(call.message.chat.id, 'Pre_intermediate video', reply_markup=one_button('https://t.me/+g-A5aWUCkW4zMTIy'))
 
     if call.data == 'intermediate':
 
-        bot.send_message(call.message.chat.id, 'Intermediate audio', reply_markup=one_button('https://t.me/+i39y2WNngXFmM2Yy'))
-        bot.send_message(call.message.chat.id, 'Intermediate video', reply_markup=one_button('https://t.me/+LxKixadK_mYxMDgy'))
+        bot.send_message(call.message.chat.id, 'Intermediate audio', reply_markup=one_button('https://t.me/+Xg8G7wG_aEZiZTBi'))
+        bot.send_message(call.message.chat.id, 'Intermediate video', reply_markup=one_button('https://t.me/+8oUE3y4W-aEyZmQy'))
 
     if call.data == 'upper_intermediate':
 
-        bot.send_message(call.message.chat.id, 'Upper_intermediate audio', reply_markup=one_button('https://t.me/+i39y2WNngXFmM2Yy'))
-        bot.send_message(call.message.chat.id, 'Upper_intermediate video', reply_markup=one_button('https://t.me/+LxKixadK_mYxMDgy'))
+        bot.send_message(call.message.chat.id, 'Upper_intermediate audio', reply_markup=one_button('https://t.me/+LLkwBZlEqMxkMDUy'))
+        bot.send_message(call.message.chat.id, 'Upper_intermediate video', reply_markup=one_button('https://t.me/+0NSqgFTFIV5jYjFi'))
 
     if call.data == 'Advanced':
 
-        bot.send_message(call.message.chat.id, 'Advanced audio', reply_markup=one_button('https://t.me/+i39y2WNngXFmM2Yy'))
-        bot.send_message(call.message.chat.id, 'Advanced video', reply_markup=one_button('https://t.me/+LxKixadK_mYxMDgy'))
+        bot.send_message(call.message.chat.id, 'Advanced audio', reply_markup=one_button('https://t.me/+LLkwBZlEqMxkMDUy'))
+        bot.send_message(call.message.chat.id, 'Advanced video', reply_markup=one_button('https://t.me/+KWnhBJhTFTAwNWUy'))
 
     else:
         return False
